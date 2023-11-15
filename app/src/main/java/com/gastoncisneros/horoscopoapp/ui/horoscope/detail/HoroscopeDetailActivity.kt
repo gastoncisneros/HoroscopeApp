94639bd7e5198f3df837ab9673e429e91b24bcab
Gaston Cisneros <gcisneros@e-verse.co>
2023-11-14 21:10:54 -0300

    SafeArgs added and new activity basic configuration

--- app/src/main/java/com/gastoncisneros/horoscopoapp/ui/horoscope/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/gastoncisneros/horoscopoapp/ui/horoscope/detail/HoroscopeDetailActivity.kt
@@ -2,11 +2,21 @@ package com.gastoncisneros.horoscopoapp.ui.horoscope.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.gastoncisneros.horoscopoapp.R
+import com.gastoncisneros.horoscopoapp.databinding.ActivityHoroscopeDetailBinding
+import com.gastoncisneros.horoscopoapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_horoscope_detail)
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
